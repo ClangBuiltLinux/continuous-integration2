b2432dcec3e9a49493b6f1758da2bb828eaceb5d
Nathan Chancellor <nathan@kernel.org>
2021-10-19 17:20:20 -0700

    ci: Restructure check-clang.sh
    Currently, check-clang.sh only checks that clang was built within the
    past five days. A follow up change will add the ability to print the
    info about the toolchain, such as the date it was built in a more human
    readable format at the hash that it was built at.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ parse-debian-clang.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+set -x
+
+parse_parameters() {
+    while [[ ${#} -gt 0 ]]; do
+        case ${1} in
+            -c | --check) action=check ;;
+        esac
+        shift
+    done
+
+    # Default action is check
+    [[ -z ${action} ]] && action=check
+}
+
+parse_clang_version() {
+    # The format of clang --version with apt.llvm.org builds looks like:
+    # $ clang-9 --version
+    # clang version 9.0.0-svn356030-1~exp1+0~20190313082415.405~1.gbp505100 (trunk)
+    # Target: x86_64-pc-linux-gnu
+    # Thread model: posix
+    # InstalledDir: /usr/bin
+    clang_date=$(clang --version | head -n1 | cut -d '~' -f 3 | cut -d . -f 1)
+
+    # Next, we need to parse the date into a format the date binary can understand
+    # We use bash substring expansion: https://wiki.bash-hackers.org/syntax/pe#substring_expansion
+    clang_date="${clang_date:0:4}-${clang_date:4:2}-${clang_date:6:2} ${clang_date:8:2}:${clang_date:10:2}:${clang_date:12:2}"
+}
+
+check_action() {
+    # Calculate the seconds between now and when Clang was built
+    # 'date -u' ensures that we get an accurate measurement because clang's date is in UTC
+    seconds_between=$(($(date -u +%s) - $(date -u -d "${clang_date:?}" +%s)))
+
+    # Then convert that to days
+    days_since=$((seconds_between / 86400))
+
+    # Error out if days_since is greater than five days
+    if [[ ${days_since} -ge 5 ]]; then
+        set +x
+        echo
+        echo "clang hasn't been updated in ${days_since} days!"
+        echo
+        exit 1
+    fi
+}
+
+parse_parameters "${@}"
+parse_clang_version
+"${action}"_action
