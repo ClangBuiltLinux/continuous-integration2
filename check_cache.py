277ae6d0793ce15bdb854b9d9bfa98461a53fbb8
Justin Stitt <jstitt007@gmail.com>
2023-12-19 14:35:36 -0800

    CI Caching  (#664)
    Signed-off-by: Justin Stitt <justinstitt@google.com>
    Co-authored-by: Justin Stitt <justinstitt@google.com>
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

---
+++ check_cache.py
@@ -0,0 +1,249 @@
+"""
+Don't run this script directly, let the CI invoke it and determine whether a
+workflow should run or not.
+
+The workflow will run if any of the following conditions are true:
+    1) This Workflow Name is not present in our Repository Variables
+    2) The Workflow Name is present but the "linux_sha" or "clang_version"
+       do not match the current versions (cache miss)
+    3) The cached build_status is not pass or fail
+
+The workflow will _not_ run if the following condition is true:
+    1) A Repository Variable with a key matching our workflow name is found
+       and it has fields "linux_sha" and "clang_version" matching the current
+       run (cache hit)
+    2) The cached build_status is pass or fail
+
+An exit code of '0' means that no Tuxsuite jobs will run while an exit code of
+'1' means that Tuxsuite will proceed.
+
+In either case, a Repository Variable with a key matching this Workflow Name
+will be created/updated matching the current linux_sha and clang_version
+detected. This ensures that our next run has the highest possible chance of a
+cache hit.
+
+When a new cache entry is created, it will have a build_status of "presuite"
+to signify that it has yet to be built by Tuxsuite. Upon completion of Tuxsuite,
+a new build_status is assigned to the cache entry by check_logs.py. Importantly,
+we want to only cache on a "pass" or "fail" status as these mean that Tuxsuite
+actually completed its work and didnt timeout.
+"""
+import argparse
+import json
+import os
+import re
+import subprocess
+import sys
+from typing import Optional
+
+import requests
+
+from utils import get_workflow_name_to_var_name, update_repository_variable
+
+OWNER = "ClangBuiltLinux"
+REPO = "continuous-integration2"
+MAIN_BRANCH = "main"
+HEADERS = {}  # populated after args are parsed
+
+# states we allow our cache system to perform a cache-hit upon
+# other states like 'presuite' or 'unknown' or '' (empty) are considered
+# poisoned and untrustworthy so we should launch the builds.
+# check_logs.py is responsible for populating the build_status field of the cache
+CACHE_HITABLE_STATES = ("pass", "fail")
+TIMEOUT = 64
+
+
+class MalformedCacheError(Exception):
+    ...
+
+
+def parse_args():
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("-g", "--github-token", required=True, type=str)
+    parser.add_argument("-w", "--workflow-name", required=True, type=str)
+    parser.add_argument("-o", "--git-repo", required=True, type=str)  # url
+    parser.add_argument("-r", "--git-ref", required=True, type=str)
+    parser.add_argument("--purge-cache", required=False, action="store_true")
+
+    return parser.parse_args()
+
+
+def get_sha_from_git_ref(git_repo: str, git_ref: str):
+    res = subprocess.run(
+        ["git", "ls-remote", git_repo, "--git-ref", git_ref],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    pattern = r"^[0-9a-z]*"
+
+    if (match := re.search(r"^[0-9a-z]*", res.stdout)) is None:
+        print(
+            f"Could not get git sha from tree {git_repo} at ref {git_ref}.\n"
+            f"Subprocess returned: {res.stdout}\n"
+            f"Which doesn't have any matches with this pattern: {pattern}\n"
+            f"Expecting something like this: be59bee58790f9d137cfc11973e856e4f8ab3888	refs/tags/v6.7-rc5"
+        )
+        sys.exit(1)
+
+    return match.group()
+
+
+def ___purge___cache___():
+    """!!!completely clears the CBL CI cache!!!"""
+    list_url = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/variables"
+
+    list_response = requests.get(list_url, headers=HEADERS, timeout=TIMEOUT)
+    print(list_response.content)
+    all_variables_keys = [
+        x["name"] for x in json.loads(list_response.content)["variables"]
+        if x["name"].startswith("_")
+    ]
+
+    for key in all_variables_keys:
+        delete_url = (
+            f"https://api.github.com/repos/{OWNER}/{REPO}/actions/variables/{key}"
+        )
+        delete_response = requests.delete(delete_url,
+                                          headers=HEADERS,
+                                          timeout=TIMEOUT)
+        if delete_response.status_code != 204:
+            print(f"ERROR: Couldn't delete cache entry with key {key}")
+            sys.exit(1)
+
+    print("CACHE CLEARED")
+
+
+def get_clang_version():
+    """
+    Get the current clang version info string.
+
+    When check_cache.py is run by a Github workflow we are in a container
+    using the specific toolchain version that Tuxmake plans to use in its
+    upcoming build(s).
+    """
+    res = subprocess.run(
+        ["clang", "--version"],
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+
+    return res.stdout.splitlines()[0].strip()
+
+
+def get_repository_variable_or_none(name: str) -> Optional[dict]:
+    _url = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/variables/{name}"
+
+    response = requests.get(_url, headers=HEADERS, timeout=TIMEOUT)
+    if response.status_code != 200:
+        return None
+
+    as_dict = json.loads(response.content.decode())
+    return json.loads(as_dict["value"])
+
+
+def create_repository_variable(name: str, linux_sha: str,
+                               clang_version: str) -> None:
+    _url = f"https://api.github.com/repos/{OWNER}/{REPO}/actions/variables"
+
+    _value = json.dumps({
+        "linux_sha": linux_sha,
+        "clang_version": clang_version,
+        "build_status": "presuite",
+    })
+    data = {"name": name, "value": _value}
+
+    resp = requests.post(_url, headers=HEADERS, json=data, timeout=TIMEOUT)
+    print(f"create_repository_variable() response:\n{resp.content}")
+
+
+if __name__ == "__main__":
+    args = parse_args()
+
+    HEADERS = {
+        "Accept": "application/vnd.github+json",
+        "Authorization": f"Bearer {args.github_token}",
+        "X-GitHub-Api-Version": "2022-11-28",
+    }
+
+    if args.purge_cache:
+        ___purge___cache___()
+        sys.exit(1)
+
+    VAR_NAME = get_workflow_name_to_var_name(args.workflow_name)
+
+    curr_sha = get_sha_from_git_ref(args.git_repo, args.git_ref)
+    curr_clang_version = get_clang_version()
+    print(
+        f"Current sha: {curr_sha}\nCurrent Clang Version: {curr_clang_version}"
+    )
+
+    # pull down repo variable
+    result = get_repository_variable_or_none(VAR_NAME)
+    if result is None:
+        print(f"CACHE MISS: Did not find repo variable {VAR_NAME} "
+              f"from workflow_name: {args.workflow_name}. Creating it now.")
+        create_repository_variable(
+            VAR_NAME,
+            linux_sha=curr_sha,
+            clang_version=curr_clang_version,
+        )
+        sys.exit(1)
+
+    # excess fields are OK but these fields are mandatory for caching.
+    missing_fields = []
+    for field in ("linux_sha", "clang_version", "build_status"):
+        if field not in result:
+            missing_fields.append(field)
+
+    if len(missing_fields) > 0:
+        raise MalformedCacheError(
+            f"The cache with key {VAR_NAME} based on workflow '{args.workflow_name}' "
+            f"is one or more fields. It's missing: {missing_fields}\n"
+            f"The current cache looks as follows:\n{result}.")
+
+    cached_sha = result["linux_sha"]
+    cached_clang_version = result["clang_version"]
+    cached_build_status = result["build_status"]
+
+    if cached_sha != curr_sha or cached_clang_version != curr_clang_version:
+        print(
+            f"CACHE MISS: current linux_sha is {curr_sha} and clang_version is {curr_clang_version} "
+            f"while {args.workflow_name} has a cached linux_sha of {cached_sha} "
+            f"and a cached clang_version of {cached_clang_version} under "
+            f"Repository Variable key: {VAR_NAME}\nUpdating cache now.")
+        update_repository_variable(
+            VAR_NAME,
+            http_headers=HEADERS,
+            sha=curr_sha,
+            clang_version=curr_clang_version,
+            build_status="presuite",
+        )
+        sys.exit(1)
+
+    # we cache hit, but we only want to allow certain states to be cacheable
+    # other states are dodgy and we're better off rerunning the builds just in case
+    if (stripped := cached_build_status.strip()) not in CACHE_HITABLE_STATES:
+        print(
+            f"CACHE HIT: Both the linux_sha and the clang_version match\n"
+            f"However, the previous build status ({stripped}) is not a status "
+            f"that check_cache.py is configured to support. The status should be "
+            f"one of {CACHE_HITABLE_STATES}.\nRunning the Tuxsuite builds now."
+        )
+        sys.exit(1)
+
+    print(
+        f"CACHE HIT: Both the linux_sha and the clang_version match\n"
+        f"CACHE:  {cached_sha} | {cached_clang_version}\nACTUAL: {curr_sha} | {curr_clang_version}\n"
+        f"Not running this workflow as it would be redundant.\n"
+        f"CACHED STATUS: {cached_build_status}")
+
+    env_file = os.getenv("GITHUB_ENV", None)
+    if env_file is not None:
+        with open(env_file, "a", encoding="utf-8") as fd:
+            fd.write(f"CACHE_PASS={cached_build_status.strip()}")
+
+    sys.exit(
+        0)  # signifies to the workflow that no jobs should run ('success')
---
+++ update_cache.py
@@ -0,0 +1,88 @@
+"""
+Run within a kick_tuxsuite_foo job.
+
+Analyze the outputted builds.json and update our cache accordingly.
+
+If _all_ builds passed we can safely update our cache with a "pass" build_status.
+
+However, if any build fails or times out, we will mark it as either "fail" or
+"badtux", respectively.
+
+To clarify "badtux", it just means Tuxsuite had an issue outside of just building
+its targets; something like a timeout or instance crash. In these instances,
+"badtux" (or any non "pass" or "fail") build status signifies to our caching
+system to not cache. The idea being that we want to rerun these jobs and not have
+a caching system stop us from doing so -- even if sha and version match.
+"""
+
+import json
+import os
+import sys
+from pathlib import Path
+
+from utils import get_workflow_name_to_var_name, update_repository_variable
+
+if "GITHUB_WORKFLOW" not in os.environ:
+    print("Couldn't find GITHUB_WORKFLOW in env. Not in a GitHub Workflow?")
+    sys.exit(1)
+
+MOCK = "MOCK" in os.environ
+
+
+def update_cache(status: str, git_sha: str, clang_version: str):
+    print(f"Trying to update cache with status: {status}")
+    cache_entry_key = get_workflow_name_to_var_name(
+        os.environ["GITHUB_WORKFLOW"])
+
+    if 'REPO_SCOPED_PAT' not in os.environ:
+        print(
+            "Couldn't find REPO_SCOPED_PAT in env. Not in a GitHub Workflow?")
+        sys.exit(1)
+
+    headers = {"Authorization": f"Bearer {os.environ['REPO_SCOPED_PAT']}"}
+
+    update_repository_variable(
+        cache_entry_key,
+        http_headers=headers,
+        build_status=status,
+        sha=git_sha,
+        clang_version=clang_version,
+        # prevent overriding a 'fail' to a 'pass'
+        allow_fail_to_pass=False)
+
+
+def main():
+    builds_json = Path(("mock." if MOCK else "") + "builds.json")
+
+    print(f"Reading {builds_json}")
+    raw = builds_json.read_text(encoding='utf-8')
+
+    builds = json.loads(raw)["builds"]
+
+    if len(builds) == 0:
+        print("No builds present. Did Tuxsuite run?")
+        sys.exit(1)
+
+    # let's grab sha and version info as Tuxsuite has the most up-to-date info
+    # we only need the first entry
+    first_entry = builds[next(iter(builds))]
+    git_sha = first_entry["git_sha"]
+    clang_version = first_entry["tuxmake_metadata"]["compiler"]["version_full"]
+
+    print(f"Tuxsuite {git_sha = } | {clang_version = }")
+
+    for _, info in builds.items():
+        if info["tuxbuild_status"] != "complete":
+            update_cache("badtux", git_sha, clang_version)
+            sys.exit(0)
+        if (status := info["build_status"]) != "pass":
+            update_cache(status, git_sha, clang_version)
+            sys.exit(0)
+
+    # only if all builds completed and passed will we set this status
+    update_cache("pass", git_sha, clang_version)
+    sys.exit(0)
+
+
+if __name__ == "__main__":
+    main()
