3d9abf14d38bea834b710ab63685ed9ab257b1e4
Justin Stitt <justinstitt@google.com>
2024-01-25 20:44:14 +0000

    run yapf -i update_cache.py

--- update_cache.py
+++ update_cache.py
@@ -31,10 +31,12 @@ MOCK = "MOCK" in os.environ
 
 def update_cache(status: str, git_sha: str, clang_version: str):
     print(f"Trying to update cache with status: {status}")
-    cache_entry_key = get_workflow_name_to_var_name(os.environ["GITHUB_WORKFLOW"])
+    cache_entry_key = get_workflow_name_to_var_name(
+        os.environ["GITHUB_WORKFLOW"])
 
     if "REPO_SCOPED_PAT" not in os.environ:
-        print("Couldn't find REPO_SCOPED_PAT in env. Not in a GitHub Workflow?")
+        print(
+            "Couldn't find REPO_SCOPED_PAT in env. Not in a GitHub Workflow?")
         sys.exit(1)
 
     headers = {"Authorization": f"Bearer {os.environ['REPO_SCOPED_PAT']}"}
@@ -69,23 +71,22 @@ def main():
     for entry, build in builds.items():
         try:
             git_sha = build["git_sha"]
-            clang_version = build["tuxmake_metadata"]["compiler"]["version_full"]
+            clang_version = build["tuxmake_metadata"]["compiler"][
+                "version_full"]
         except KeyError:
             builds_that_are_missing_metadata.append(entry)
 
     if len(builds_that_are_missing_metadata) == len(builds):
         raise RuntimeError(
             f"Could not find a suitable git sha or compiler version in any build\n"
-            f"Here's the build.json:\n{raw}"
-        )
+            f"Here's the build.json:\n{raw}")
 
     if len(builds_that_are_missing_metadata) > 0:
         print(
             "Warning: Some of the builds in builds.json are malformed and missing "
             "some metadata.\n"
             f"Here's a list: {builds_that_are_missing_metadata}\n"
-            f"Here's the build.json in question:\n{raw}"
-        )
+            f"Here's the build.json in question:\n{raw}")
         sys.exit(0)
 
     assert git_sha and clang_version
