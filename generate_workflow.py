86ce823a588637397ad146c53c9da7afce0be537
Nathan Chancellor <nathan@kernel.org>
2024-01-26 12:18:44 -0700

    generate_workflow.py: Remove unnecessary make_variables chck in get_job_name()
    The next line unconditionally uses build["make_variables"], which makes
    the check for make_variables useless, as there would be an exception if
    it were not present.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- generate_workflow.py
+++ generate_workflow.py
@@ -71,7 +71,7 @@ def get_job_name(build):
     else:
         job += " CC=clang"
     # If LD was specified, show what it is
-    if "make_variables" in build and "LD" in build["make_variables"]:
+    if "LD" in build["make_variables"]:
         job += " LD=" + str(build["make_variables"]["LD"])
     job += " LLVM_IAS=" + str(build["make_variables"]["LLVM_IAS"])
     # Having "LLVM <VER>" is a little hard to parse, make it look like
