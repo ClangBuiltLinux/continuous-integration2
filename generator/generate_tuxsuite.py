d68a75c5787ef6ee74c57b711e8f91b988ffec6b
Nathan Chancellor <nathan@kernel.org>
2026-02-23 12:53:41 -0700

    generator: Run Python scripts with uv by default
    uv [1] is a Python package manager that allows running scripts with
    dependencies in dedicated virtual environments. This allows scripts to
    work consistently across all workspaces and environments as long as uv
    is available (and it is very easy to install [2]), rather than manually
    installing packages or managing virtual environments.
    
    Convert all Python scripts to run with uv by default by adding their
    dependencies with
    
      $ uv add --script <file> pyyaml
    
    and adjusting the shebang accordingly. This does not prevent the scripts
    from being run manually using a Python interpreter directly, it just
    provides a better default experience.
    
    Link: https://github.com/astral-sh/uv [1]
    Link: https://docs.astral.sh/uv/getting-started/installation/ [2]
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- generator/generate_tuxsuite.py
+++ generator/generate_tuxsuite.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.8"
+# dependencies = [
+#     "pyyaml>=6.0.3",
+# ]
+# ///
 import argparse
 from pathlib import Path
 import sys
